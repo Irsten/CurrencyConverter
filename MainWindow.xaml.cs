e2643bc0bd41679b10215a44b6be828a2d08b6c4
Irsten <irsten3@gmail.com>
2021-12-21 18:19:51 +0100

    Add database

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -13,14 +15,83 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Newtonsoft.Json;
 
 namespace CurrencyConverter
 {
     public partial class MainWindow : Window
     {
+        Root val = new Root();
         public MainWindow()
         {
             InitializeComponent();
+            GetValue();
+        }
+        private async void GetValue()
+        {
+            val = await GetData<Root>("https://openexchangerates.org/api/latest.json?app_id=f33fe06dc28547858c1dc801cd059d54");
+            BindCurrency();
+        }
+        public static async Task<Root> GetData<T>(string url)
+        {
+            var myRoot = new Root();
+            try
+            {
+                // HttpClient class provides a base class for sending/recieving the HTTP request/responses for URL
+                using (var client = new HttpClient())
+                {
+                    // The timespan to wait before the request times out    
+                    client.Timeout = TimeSpan.FromMinutes(1);
+                    // HttpResponseMessage is a way of returning a message/date from your action
+                    HttpResponseMessage response = await client.GetAsync(url);
+                    // Check API response code ok 
+                    if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        // Serialize the HTTP content to a string as an asynchronous operation
+                        var ResponceString = await response.Content.ReadAsStringAsync();
+                        // JsonConvert.DeserializeObject to deserialize Json to a C#
+                        var ResponceObject = JsonConvert.DeserializeObject<Root>(ResponceString);
+
+                        // Return API responce
+                        return ResponceObject;
+                    }
+                    return myRoot;
+                }
+            }
+            catch
+            {
+                return myRoot;
+            }
+        }
+        private void BindCurrency()
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Currency");
+            dt.Columns.Add("Value");
+
+            // Add rows in the Datatable
+            dt.Rows.Add("SELECT CURRENCY", 0);
+            dt.Rows.Add("PLN", val.rates.PLN);
+            dt.Rows.Add("USD", val.rates.USD);
+            dt.Rows.Add("EUR", val.rates.EUR);
+            dt.Rows.Add("GBP", val.rates.GBP);
+            dt.Rows.Add("JPY", val.rates.JPY);
+            dt.Rows.Add("CHF", val.rates.CHF);
+            dt.Rows.Add("INR", val.rates.INR);
+            dt.Rows.Add("NZD", val.rates.NZD);
+            dt.Rows.Add("ISK", val.rates.ISK);
+
+            // DataTable data asigned from the From combobox
+            cmbFrom.ItemsSource = dt.DefaultView;
+            cmbFrom.DisplayMemberPath = "Currency";
+            cmbFrom.SelectedValuePath = "Value";
+            cmbFrom.SelectedIndex = 0;
+
+            // DataTable data asigned from the To combobox
+            cmbTo.ItemsSource = dt.DefaultView;
+            cmbTo.DisplayMemberPath = "Currency";
+            cmbTo.SelectedValuePath = "Value";
+            cmbTo.SelectedIndex = 0;
         }
         private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
         {
